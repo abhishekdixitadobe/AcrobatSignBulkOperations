ca5a304400ad59041878baf1c9af3788f9ccd73a
Abhishek Dixit <abhishekd@adobe.com>
2023-04-18 12:41:21 +0530

    added JS

---
+++ src/main/resources/static/styles/js/validation.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+        $("#selectAll").change(function () {
+            $("input:checkbox").prop('checked', $(this).prop("checked"));
+             $("#checkboxSelectError").html("").removeClass("w3-panel w3-red");
+        });
+        
+       /*$("#bulkform").on("submit", function(e) {
+	     //$("#bulkform button").click(function (e) {
+            e.preventDefault();
+             if ($('input:checked', this).length >= 1) {
+					var buttonName = e.originalEvent.submitter.id ;
+					var buttonValue = e.originalEvent.submitter.defaultValue;
+					var formaction = ($("#download").attr("name") + ':'+ $("#download").attr("value"));
+					//$(this).append(formaction);
+					var formData = $("#bulkform").serializeArray();
+					formData.push({name: buttonName, value: buttonValue});
+					$.post("/deleteagreements",formData,
+			            function(response){
+							var isErr = 'hasError';
+			                        // when there are an error then show error
+			                        if (response.indexOf(isErr) > -1) {
+			                            $("#yourPanel").html(response);
+			                        } else {
+			                           window.location.replace(window.URL);
+			                        }
+						});
+	        } else {
+	            alert('Please select at least 1 checkboxes!');
+	            return false;
+	        }
+           
+        });*/
+       $("#bulkform").on("submit", function(e) {
+			 if ($('input:checked', this).length >= 1) {
+						var buttonName = e.originalEvent.submitter.id ;
+						var buttonValue = e.originalEvent.submitter.defaultValue;
+						var formData = $("#bulkform").serializeArray();
+						formData.push({name: buttonName, value: buttonValue});
+				        $.ajax({
+					        type: "POST",
+					        contentType: "application/json",
+					        url: "/deleteagreements",
+					        data: JSON.stringify(formData),
+					        dataType: 'json',
+					        cache: false,
+					        timeout: 600000,
+					        success: function (data) {
+								console.log("SUCCESS : ", data);
+								},
+				        	error: function (e) {
+				            console.log("ERROR : ", e);
+				
+				        }
+				    });
+		   }else {
+	            //alert('Please select at least 1 checkboxes!');
+	            $("#checkboxSelectError").html("Please select at least one CheckBox").addClass("w3-panel w3-red");
+	            return false;
+	        }
+     });
+});
