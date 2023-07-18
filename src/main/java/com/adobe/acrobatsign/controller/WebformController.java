dbb013c6a9073cedcdc2d8ea076e0e19e4c4f146
Abhishek Dixit <abhishekd@adobe.com>
2023-07-18 23:06:33 +0530

    added webform export code

--- src/main/java/com/adobe/acrobatsign/controller/WebformController.java
+++ src/main/java/com/adobe/acrobatsign/controller/WebformController.java
@@ -1,20 +1,36 @@
 package com.adobe.acrobatsign.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.adobe.acrobatsign.model.AgreementForm;
+import com.adobe.acrobatsign.model.ExportWidget;
 import com.adobe.acrobatsign.model.MultiUserWidgetDetails;
+import com.adobe.acrobatsign.model.UserWidget;
 import com.adobe.acrobatsign.service.UserService;
 import com.adobe.acrobatsign.service.WebformService;
 import com.adobe.acrobatsign.util.Constants;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 @Controller
 public class WebformController {
@@ -53,6 +69,40 @@ public class WebformController {
 		model.addAttribute("activeUserList", multiUserWidgetDetails.getUserEmails());
 		model.addAttribute("agreementForm", agreementForm);
 		return "webformList";
+	}
+
+	@RequestMapping(value = Constants.DOWNLOAD_WEBFORMS, method = RequestMethod.POST, params = "downloadWebforms")
+	public void downloadWebforms(HttpServletResponse response, @RequestParam String userEmail,
+			@ModelAttribute("agreementForm") AgreementForm agreementForm) {
+
+		String filename = "WidgetList.csv";
+		response.setContentType("text/csv");
+		response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"");
+		try {
+			StatefulBeanToCsv<ExportWidget> writer = new StatefulBeanToCsvBuilder<ExportWidget>(response.getWriter())
+					.withQuotechar(CSVWriter.NO_QUOTE_CHARACTER).withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+					.withOrderedResults(false).build();
+
+			writer.write(seletedList(agreementForm));
+		} catch (CsvDataTypeMismatchException | CsvRequiredFieldEmptyException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private List<ExportWidget> seletedList(AgreementForm agreementForm) {
+		final List<ExportWidget> seletedList = new ArrayList<>();
+		for (final UserWidget widget : agreementForm.getWidgetList()) {
+			if (widget.getIsChecked() != null) {
+				ExportWidget exportWidget = new ExportWidget();
+				exportWidget.setId(widget.getId());
+				exportWidget.setName(widget.getName());
+				exportWidget.setOwnerEmail(widget.getOwnerEmail());
+				exportWidget.setStatus(widget.getStatus());
+				exportWidget.setUrl(widget.getUrl());
+				seletedList.add(exportWidget);
+			}
+		}
+		return seletedList;
 	}
 
 }
