0c163e46828279cbcf7f3397bd5a5d90761ec4e1
Abhishek Dixit <abhishekd@adobe.com>
2023-08-22 18:30:19 +0530

    lib tempaltes

--- src/main/java/com/adobe/acrobatsign/controller/LibraryTemplateController.java
+++ src/main/java/com/adobe/acrobatsign/controller/LibraryTemplateController.java
@@ -5,12 +5,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,9 +16,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -36,39 +30,23 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 
 import com.adobe.acrobatsign.model.AgreementForm;
 import com.adobe.acrobatsign.model.LibraryDocument;
-import com.adobe.acrobatsign.model.LibraryDocuments;
 import com.adobe.acrobatsign.model.MultiUserAgreementDetails;
-import com.adobe.acrobatsign.model.UserAgreement;
-import com.adobe.acrobatsign.model.UserWorkflows;
 import com.adobe.acrobatsign.service.LibraryTemplateService;
 import com.adobe.acrobatsign.util.Constants;
 
 @Controller
 public class LibraryTemplateController {
-	
-	/** The adobe sign service. */
-	@Autowired
-	LibraryTemplateService libraryTemplateService;
-	
+
 	/** The Constant LOGGER. */
 	private static final Logger LOGGER = LoggerFactory.getLogger(LibraryTemplateController.class);
 
+	/** The adobe sign service. */
+	@Autowired
+	LibraryTemplateService libraryTemplateService;
+
 	@Value("${pageSize}")
 	public String maxLimit;
-	
-	@RequestMapping(value = Constants.DELETE_AGREEMENTS, method = RequestMethod.POST, params = Constants.DOWNLOAD_TEMPLATE_PARAM)
-	public ResponseEntity<StreamingResponseBody> downloadTemplates(HttpServletResponse response,
-			@RequestParam String userEmail, @ModelAttribute(Constants.AGREEMENT_FORM) AgreementForm agreementForm) {
-		final StreamingResponseBody streamResponseBody = out -> {
-			libraryTemplateService.downloadTemplates(seletedList(agreementForm), userEmail, response);
-		};
-		response.setContentType("application/zip");
-		response.setHeader("Content-Disposition", "attachment; filename=agreements.zip");
-		response.addHeader("Pragma", "no-cache");
-		response.addHeader("Expires", "0");
-		return new ResponseEntity(streamResponseBody, HttpStatus.OK);
-	}
-	
+
 	@RequestMapping(value = Constants.DELETE_AGREEMENTS, method = RequestMethod.POST, params = Constants.FORM_FIELD_TEMPLATE)
 	public ResponseEntity<StreamingResponseBody> downloadformfields(HttpServletResponse response,
 			@RequestParam String userEmail, @ModelAttribute(Constants.AGREEMENT_FORM) AgreementForm agreementForm,
@@ -83,29 +61,20 @@ public class LibraryTemplateController {
 		response.addHeader("Expires", "0");
 		return ResponseEntity.ok(streamResponseBody);
 	}
-	
-	private List<LibraryDocument> seletedList(AgreementForm agreementForm) {
-		final List<LibraryDocument> seletedList = new ArrayList<>();
-		for (final LibraryDocument document : agreementForm.getLibraryTemplateList()) {
-			if (document.getIsChecked() != null) {
-				seletedList.add(document);
-			}
-		}
-		return seletedList;
+
+	@RequestMapping(value = Constants.DELETE_AGREEMENTS, method = RequestMethod.POST, params = Constants.DOWNLOAD_TEMPLATE_PARAM)
+	public ResponseEntity<StreamingResponseBody> downloadTemplates(HttpServletResponse response,
+			@RequestParam String userEmail, @ModelAttribute(Constants.AGREEMENT_FORM) AgreementForm agreementForm) {
+		final StreamingResponseBody streamResponseBody = out -> {
+			libraryTemplateService.downloadTemplates(seletedList(agreementForm), userEmail, response);
+		};
+		response.setContentType("application/zip");
+		response.setHeader("Content-Disposition", "attachment; filename=agreements.zip");
+		response.addHeader("Pragma", "no-cache");
+		response.addHeader("Expires", "0");
+		return new ResponseEntity(streamResponseBody, HttpStatus.OK);
 	}
-	
-	@RequestMapping(value = Constants.DELETE_AGREEMENTS, method = RequestMethod.POST, params = Constants.HIDE_TEMPLATE)
-	public String hideTemplates(Model model, @ModelAttribute(Constants.AGREEMENT_FORM) AgreementForm agreementForm){
-		libraryTemplateService.hideTemplates(seletedList(agreementForm));
-		model.addAttribute(Constants.AGREEMENT_FORM, agreementForm);
-		return Constants.RETURN_LIBRARY_TEMPLATE;
-	}
-	
-	@RequestMapping(value = Constants.SEARCH_LIBRARY_TEMPLATE, method = RequestMethod.GET)
-	public String searchLibraryTemplate(Model model) {
-		return Constants.RETURN_LIBRARY_TEMPLATE;
-	}
-	
+
 	@PostMapping(Constants.FETCH_TEMPLATE)
 	public String fetchUsersTemplate(Model model, @RequestParam(Constants.PARAM_FILE) MultipartFile file1,
 			@RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size) {
@@ -121,18 +90,18 @@ public class LibraryTemplateController {
 				e.printStackTrace();
 			}
 		}
-		
-		//New code
+
+		// New code
 
 		MultiUserAgreementDetails multiUserAgreementDetails = libraryTemplateService.fetchLibraryTemplate(userIds);
-		
-		//New code start
+
+		// New code start
 		final long totalTemplates = multiUserAgreementDetails.getTotalTemplates();
 		agreementForm.setAgreementIdList(multiUserAgreementDetails.getAgreementList());
 		agreementForm.setLibraryTemplateList(multiUserAgreementDetails.getLibraryDocumentList());
 		agreementForm.setTotalAgreements(multiUserAgreementDetails.getTotalAgreements());
 		multiUserAgreementDetails.setNextIndexMap(multiUserAgreementDetails.getNextIndexMap());
-		
+
 		model.addAttribute(Constants.USER_IDS, multiUserAgreementDetails.getUserEmails());
 		if (multiUserAgreementDetails.getUserEmails().size() > 1) {
 			model.addAttribute(Constants.USER_EMAIL, multiUserAgreementDetails.getUserEmails().get(1));
@@ -143,11 +112,32 @@ public class LibraryTemplateController {
 		model.addAttribute(Constants.TOTAL_TEMPLATES, multiUserAgreementDetails.getTotalTemplates());
 		model.addAttribute(Constants.AGREEMENT_FORM, agreementForm);
 		model.addAttribute(Constants.NEXT_INDEX_MAP, multiUserAgreementDetails.getNextIndexMap());
-		//New code end
+		// New code end
 		model.addAttribute(Constants.LIBRARY_TEMPLATE_LIST, multiUserAgreementDetails.getLibraryDocumentList());
 		return Constants.LIBRARY_TEMPLATES;
 
-		
+	}
+
+	@RequestMapping(value = Constants.DELETE_AGREEMENTS, method = RequestMethod.POST, params = Constants.HIDE_TEMPLATE)
+	public String hideTemplates(Model model, @ModelAttribute(Constants.AGREEMENT_FORM) AgreementForm agreementForm) {
+		libraryTemplateService.hideTemplates(seletedList(agreementForm));
+		model.addAttribute(Constants.AGREEMENT_FORM, agreementForm);
+		return Constants.LIBRARY_TEMPLATES;
+	}
+
+	@RequestMapping(value = Constants.SEARCH_LIBRARY_TEMPLATE, method = RequestMethod.GET)
+	public String searchLibraryTemplate(Model model) {
+		return Constants.RETURN_LIBRARY_TEMPLATE;
+	}
+
+	private List<LibraryDocument> seletedList(AgreementForm agreementForm) {
+		final List<LibraryDocument> seletedList = new ArrayList<>();
+		for (final LibraryDocument document : agreementForm.getLibraryTemplateList()) {
+			if (document.getIsChecked() != null) {
+				seletedList.add(document);
+			}
+		}
+		return seletedList;
 	}
 
 }
