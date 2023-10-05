86821c50b7538f625e5db2d7cb6f25a3b5271454
Abhishek Dixit <abhishekd@adobe.com>
2023-10-05 13:32:17 +0530

    update flow to include page size

--- src/main/java/com/adobe/acrobatsign/model/AgreementAssetsCriteria.java
+++ src/main/java/com/adobe/acrobatsign/model/AgreementAssetsCriteria.java
@@ -3,7 +3,7 @@ package com.adobe.acrobatsign.model;
 import java.util.List;
 
 public class AgreementAssetsCriteria {
-	
+
 	private List<String> status;
 	private List<String> type;
 	private DateRangeFilter modifiedDate;
@@ -11,48 +11,70 @@ public class AgreementAssetsCriteria {
 	private String visibility;
 	private Integer startIndex;
 	private List<String> groupId;
-	
+	private Integer pageSize;
+
 	public List<String> getGroupId() {
 		return groupId;
 	}
-	public void setGroupId(List<String> groupId) {
-		this.groupId = groupId;
-	}
-	public List<String> getRole() {
-		return role;
-	}
-	public void setRole(List<String> role) {
-		this.role = role;
-	}
-	public List<String> getStatus() {
-		return status;
-	}
-	public void setStatus(List<String> status) {
-		this.status = status;
-	}
-	public List<String> getType() {
-		return type;
-	}
-	public void setType(List<String> type) {
-		this.type = type;
-	}
+
 	public DateRangeFilter getModifiedDate() {
 		return modifiedDate;
 	}
-	public void setModifiedDate(DateRangeFilter modifiedDate) {
-		this.modifiedDate = modifiedDate;
+
+	public Integer getPageSize() {
+		return pageSize;
 	}
-	public String getVisibility() {
-		return visibility;
+
+	public List<String> getRole() {
+		return role;
 	}
-	public void setVisibility(String visibility) {
-		this.visibility = visibility;
-	}
+
 	public Integer getStartIndex() {
 		return startIndex;
 	}
+
+	public List<String> getStatus() {
+		return status;
+	}
+
+	public List<String> getType() {
+		return type;
+	}
+
+	public String getVisibility() {
+		return visibility;
+	}
+
+	public void setGroupId(List<String> groupId) {
+		this.groupId = groupId;
+	}
+
+	public void setModifiedDate(DateRangeFilter modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public void setRole(List<String> role) {
+		this.role = role;
+	}
+
 	public void setStartIndex(Integer startIndex) {
 		this.startIndex = startIndex;
+	}
+
+	public void setStatus(List<String> status) {
+		this.status = status;
+	}
+
+	public void setType(List<String> type) {
+		this.type = type;
+	}
+
+	public void setVisibility(String visibility) {
+		this.visibility = visibility;
 	}
 
 }
